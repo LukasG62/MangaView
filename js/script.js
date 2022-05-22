95f45cf0f5cd00e3c0a0e92cfb8b78390fbb78f3
LukasG62 <98223802+LukasG62@users.noreply.github.com>
2022-05-22 17:06:20 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+// Fonction qui permet de cacher ou d'afficher un spoiler
+function toggle_spoiler(ref) {
+
+    if(ref.className == "spoiler-hidden") ref.className = "spoiler-shown"
+    else ref.className = "spoiler-hidden"
+}
