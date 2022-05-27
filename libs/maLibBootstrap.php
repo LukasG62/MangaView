389f6bb97fedfb3f2ec2c6725778c215d33a2591
LukasG62 <lukas.grando.62@gmail.com>
2022-05-27 23:44:52 +0200

    ajout mkReview et modif rêgles css

--- libs/maLibBootstrap.php
+++ libs/maLibBootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
 include_once "modele.php";
+include_once "LibMangaview.php";
+
 /*
 Ce fichier définit diverses fonctions permettant de faciliter la production de mises en formes complexes
 Il est utilisé en conjonction avec le style de bootstrap et insère des classes bootstrap
@@ -31,8 +33,10 @@ function mkError($message) {
 }
 
 function mkNews($dataNews, $active="") { // TODO : banner à adapter pour les chemins relatifs
+  global $uploadInfo;
+
   $news = '<div class="carousel-item ' . $active . '"><div class="news-preview">'.
-  		  '<a href="index.php?view=news&id=' . $dataNews["id"] . '"><img class="img-fluid" src="' . $dataNews["banner"]. '" /></a>' .
+  		  '<a href="index.php?view=news&id=' . $dataNews["id"] . '"><img class="img-fluid" src="' . $uploadInfo["NEWSPATH"] . $dataNews["banner"]. '" /></a>' .
           '<div class="news-preview-text"><p><i class="bi bi-clock"></i> Posté le <b>' . date_format(date_create($dataNews["date"]), "d/m/Y") . '</b> par <b>' . getUserPseudo($dataNews["uid"]) . '</b></p>' .
           '<h1>' . $dataNews["title"] . '</h1>' .
           '<p>' . bbcodeparser(htmlspecialchars($dataNews["content"])) . '</p>' . '</div></div></div>';
@@ -41,16 +45,33 @@ function mkNews($dataNews, $active="") { // TODO : banner à adapter pour les ch
 }
 
 function mkComment($dataComment) { // TODO : userAvatar à adapter pour les chemins relatifs  + faire le css pour rendre ça plus jolie
+	global $uploadInfo;
+
 	$comment = '<div class="comment-widgets m-b-20"><div class="d-flex flex-row comment-row">' .
-			   '<div class="p-2"><span class="round"><img src="' . getUserAvatar($dataComment["uid"]) . '" alt="user" width="100"></span></div>' .
+			   '<div class="p-2"><span class="round"><img src="' . $uploadInfo["USERPATH"] .getUserAvatar($dataComment["uid"]) . '" alt="user" width="100"></span></div>' .
 			   '<div class="comment-text w-100"><h5>' . getUserPseudo($dataComment["uid"]) . ' : </h5>' .
 			   '<p class="m-b-5 m-t-10">' . bbcodeParser(htmlspecialchars($dataComment["content"])) .'</p></div></div>';
 
 	return $comment;
 }
 
-function mkReview($review) {
-	// Fonction qui construit une review en html
+function mkReview($dataReview) {
+	global $uploadInfo;
+
+	$color = "color:green;";
+	if($dataReview["note"] == 5) 
+		$color = "color:darkorange;";
+	else if($dataReview["note"] < 5) $color = "color:red;";
+
+
+	$review = '<div class="mv-review container"><div class="row"><div class="col-md-2"><div class="avatar-review">' .
+			  '<a href="index.php?view=profile&id=' . $dataReview["uid"] . '">' . 
+			  '<img width="150" height="150" src="'. $uploadInfo["USERPATH"] . getUserAvatar($dataReview["uid"]) .'" alt="User avatar"/></a>' .
+			  '<h4>' . getUserPseudo($dataReview["uid"]) . '</h4>' .
+			  '<h5 style="' . $color . '">' . $dataReview["note"] . '/10</h5></div></div><div class="col-md-8"><p>' .
+			  bbcodeParser(htmlspecialchars($dataReview["content"])) . '</p></div></div>';
+
+	return $review;
 }
 
 function mkCollection($collection, $myprofile) {
