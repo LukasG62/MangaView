374eb34d17bf730194c542344e4c7015ec8e1427
LukasG62 <lukas.grando.62@gmail.com>
2022-05-28 19:37:14 +0200

    test mkSearchSeries

--- templates/series.php
+++ templates/series.php
@@ -4,4 +4,19 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 	header("Location:../index.php?view=series");
 	die("");
 }
+
+$dataSeries = ["title"=>"Spice and wolf",
+			   "id" =>"1",
+			   "status"=>0,
+			   "cover" => "1.jpg"
+];
+
 ?>
+
+
+<div class="mv-manga container">
+	<div class="row">
+		<?=mkSearchSeries($dataSeries)?>
+		<?=mkSearchSeries($dataSeries)?>
+	</div>
+</div>
