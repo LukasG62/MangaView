9bddc0da6e45aa3773349cff5e1f8fde5474dc3a
LukasG62 <lukas.grando.62@gmail.com>
2022-05-29 23:59:02 +0200

    ajout accordion

--- templates/series.php
+++ templates/series.php
@@ -34,18 +34,33 @@ if(!($page = valider("page")) || $page >= $nbPages || $page < 0) $page = 0;
       <div class="input-group mb-3">
         <?=mkInput("text", "search", "", "placeholder=\"Rechercher une séries\"class=\"form-control\"")?>
         <div class="input-group-append">
-          <?=mkButton("submit","action", "Search","Rechercher","class=\"btn btn-outline-dark\"")?>
+          <?=mkInput("submit","action", "Rechercher","class=\"btn btn-outline-dark\"")?>
         </div>
       </div>
     </div>
     <div class = "col-2">
       <div class="input-group mb-3">
         <?=mkSelect("sortby",$sortsAvailable, "id", "label", "class=\"\"", "[\"title\"]")?>
-        <?=mkButton("submit","action", "Sort","Trier","class=\"btn btn-outline-dark\"")?>
+        <?=mkInput("submit","action", "Trier","class=\"btn btn-outline-dark\"")?>
 
       </div>
     </div>
   </div>
+  <div id="tags">
+    <div class="card">
+      <div class="card-header" id="TagsHeader">
+        <h5 class="mb-0">
+          <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseTags" aria-expanded="true" aria-controls="collapseTags">Rechercher par themes :</button>
+        </h5>
+      </div>
+      <div id="collapseTags" class="collapse show" aria-labelledby="TagsHeader" data-parent="#tags">
+        <div class="card-body">
+          TAGS GOES HERE !
+        </div>
+      </div>
+    </div>
+  </div>
+
   <?=endForm()?>
 	<?=mkSearchPage($partionedSeriesList, $page, $nbPages)?>
 
