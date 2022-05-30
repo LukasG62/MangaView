cc33923c741c722bf6ba601036cdf71bc00c0214
JeromeDevienne6959 <81384168+JeromeDevienne6959@users.noreply.github.com>
2022-05-30 17:06:51 +0200

    Update newshistory.php

--- templates/newshistory.php
+++ templates/newshistory.php
@@ -4,4 +4,16 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 	header("Location:../index.php?view=newshistory");
 	die("");
 }
+
+$tabNews = getNewsDecroi();
 ?>
+
+<div class="container" >
+	<?php 
+		foreach($tabNews as $dataNews)
+		{
+			echo mkNews($dataNews,"active","");
+			echo "<br/>";
+		}
+	?>
+</div>
