52bb75aa4484e09129fc1f6b02afc093bb910ddd
AlexF <99125830+Nekotaku0@users.noreply.github.com>
2022-05-30 16:22:44 +0200

    Update tome.php

--- templates/tome.php
+++ templates/tome.php
@@ -5,3 +5,16 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 	die("");
 }
 ?>
+
+<?php
+$_tome = getVolumes(1) ;
+$_manga = getSerie($_tome[0]["mid"]);
+$_review = getReview(2);
+echo "<div class='container container-fluid'><img class='banner' src='".$uploadInfo[SERIESPATH].$_manga[0][banner]."'/></img></div>";
+echo "<div class='container tome'> <h1>".$_tome[0]["title"]."</h1> <br> <img width='150' height='200' src='".$uploadInfo[VOLUMESPATH].$_tome[0][cover]."'/><br> <p>".$_tome[0][synopsis]."<br> Date de sortie : ".$_tome[0][releaseDate]."</div> ";
+
+echo "<div class='container fixxxxeee'>";
+foreach ($_review as $rev)
+	echo mkReview($rev);
+echo "</div>";
+?>
