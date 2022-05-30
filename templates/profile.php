b17d0e4c29ac062edb89f75546f7b92bbe2a71de
JeromeDevienne6959 <81384168+JeromeDevienne6959@users.noreply.github.com>
2022-05-30 16:15:56 +0200

    Update profile.php

--- templates/profile.php
+++ templates/profile.php
@@ -1,7 +1,54 @@
 <?php
 if (basename($_SERVER["PHP_SELF"]) != "index.php")
 {
-	header("Location:../index.php?view=profile");
+	header("Location:../index.php?view=myprofile");
 	die("");
 }
+global $uploadInfo;
+$idUser = valider("idUser", "GET");
+$pseudo = getUserPseudo($idUser);
+$rank = getGradeLabel($idUser);
+$bio = getUserBio($idUser);
+$imgPath = $uploadInfo["USERSPATH"];
+$imgPath .= getUserAvatar($idUser);
 ?>
+
+
+<img src="<?=$imgPath?>" alt="User avatar" class="photoProfil">
+    <h3><?php echo($pseudo);?></h3>
+    <p>Rank : <?php echo($rank);?> </p>
+    <div class="bio">
+        <p><?php echo($bio);?></p>
+    </div>
+    <br>
+    <h4 class="floatLeft">Ma collection</h4>
+    <br>
+    <select name="Choix" id="choixTriCollection">
+        <option value="">--Please choose an  option</option>
+        <option value="Tomes">Tomes</option>
+        <option value="Manga">Manga</option>
+	</select>
+
+<br><br>
+
+    <h4 class="floatLeft">Mes Favoris</h4>
+    <br>
+    <select name="Choix" id="choixTriCollection">
+        <option value="">--Please choose an  option</option>
+        <option value="Tomes">Tomes</option>
+        <option value="Manga">Manga</option>
+    </select>
+<br><br>
+
+    <h4>Mes Reviews</h4>
+
+	<?php 
+		$tabReviewUser = getAllReview();
+		foreach($tabReviewUser as $dataReview)
+		{
+			if ($dataReview['uid'] == $idUser)
+			{
+				echo mkReview($dataReview);
+			}
+		}
+	?>
