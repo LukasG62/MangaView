9ce3fa2147c8e5f1f6f43cf4b84068f050e1bb38
Nekotaku0 <alexandre.fizel111@gmail.com>
2022-05-31 17:28:29 +0200

    UPDATE signup

--- templates/signup.php
+++ templates/signup.php
@@ -1,7 +1,48 @@
 <?php
+
+// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
 if (basename($_SERVER["PHP_SELF"]) != "index.php")
 {
-	header("Location:../index.php?view=signin");
+	header("Location:../index.php?view=login");
 	die("");
 }
+
+$msg = valider("msg");
+
 ?>
+
+<div class="mv-login-form">
+  <div class="container">
+    <?php if($msg) echo mkError($msg)?>
+    <div class="row justify-content-md-center">
+      <div class="col-6 card-wrapper">
+        <div class="card fat">
+          <div class="card-body">
+            <h4 class="card-title">Inscrivez-vous dès maintenant</h4>
+            <form action ="controleur.php" enctype="multipart/form-data" method="POST">
+              <div class="form-group">
+                <label for="username">Username :</label>
+                <input id="text-username" type="text" class="form-control" name="username" value="">
+              </div>
+              <div class="form-group">
+                <label for="password">Password : </label>
+                <input id="text-password" type="password" class="form-control" name="password">
+              </div>
+			  <div class="form-group">
+                <label for="password">Confirmmer votre password : </label>
+                <input id="text-password" type="password" class="form-control" name="newpassword">
+              </div>
+			  <div class="form-group">
+                <label for="pdp">Password : </label>
+                <input id="text-password" type="file" class="form-control" name="fileToUpload">
+              </div>
+              <div class="form-group m-0">
+                <button name="action" value="Signin" type="submit" class="btn btn-primary btn-block">Créer mon compte</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
