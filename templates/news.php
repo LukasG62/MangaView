ac55c716d4b364de9ace9efd4d00a40731d35ce8
LukasG62 <lukas.grando.62@gmail.com>
2022-06-01 01:05:29 +0200

    structure et page fonctionnel

--- templates/news.php
+++ templates/news.php
@@ -4,4 +4,28 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 	header("Location:../index.php?view=news");
 	die("");
 }
+
+$urlBase = dirname($_SERVER["PHP_SELF"]) . "/index.php";
+if(!($idNews = valider("id", "GET"))) rediriger($url,["view"=>"accueil"]);
+	
+if(!($dataNews = getNewsById($idNews)[0])) rediriger($url,["view"=>"accueil"]);
+
 ?>
+
+
+<div class="container mv-pagebase">
+	<?= mkNews($dataNews, "", "")?>
+<div>
+
+<br />
+<div class="container mv-pagebase">
+	<h2>Les commentaires :</h2> 
+	<?php
+		$dataComment = getComments($idNews, 'news');
+		foreach($dataComment as $dataOneComment){
+			$comment = mkComment($dataOneComment);
+			echo($comment);
+		}
+	?>
+</div>
+
