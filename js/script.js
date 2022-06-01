f1391316cdef8d72c7ffe09c08d4482cff85dc2d
LukasG62 <lukas.grando.62@gmail.com>
2022-06-01 16:12:13 +0200

    add toggle_fav

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ function toggle_spoiler(ref) {
     if(ref.className == "spoiler-hidden") ref.className = "spoiler-shown"
     else ref.className = "spoiler-hidden"
 }
+
+function init() {
+
+}
+
+
+function toggle_fav(ref) {
+	console.log(ref.style.backgroundImage);
+	refFavValue = document.getElementById("favValue");
+	
+	if(ref.style.backgroundImage == "url(\"ressources/img/star_empty.png\")" || ref.style.backgroundImage == "url(\"ressources/img/star_empty.png\")") {
+		ref.style.backgroundImage = "urL(\"ressources/img/star_fill.png\")"
+		refFavValue.value = 1
+		
+	}
+	else {
+		ref.style.backgroundImage = "urL(\"ressources/img/star_empty.png\")"
+		refFavValue.value = 0
+	}
+}
