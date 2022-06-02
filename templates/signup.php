3661f63b01c664faae6b9ba9ae9bab8a807206a4
LukasG62 <lukas.grando.62@gmail.com>
2022-06-02 02:00:38 +0200

    ajout vérif client-side

--- templates/signup.php
+++ templates/signup.php
@@ -21,23 +21,29 @@ $msg = valider("msg");
             <h4 class="card-title">Inscrivez-vous dès maintenant</h4>
             <form action ="controleur.php" enctype="multipart/form-data" method="POST">
               <div class="form-group">
-                <label for="username">Username :</label>
+                <label for="text-username">Username :</label>
                 <input id="text-username" type="text" class="form-control" name="username" value="">
               </div>
               <div class="form-group">
-                <label for="password">Password : </label>
+                <label for="text-password">Mot de passe : </label>
                 <input id="text-password" type="password" class="form-control" name="password">
+                <div class="invalid-feedback">
+                </div>
               </div>
 			  <div class="form-group">
-                <label for="password">Confirmmer votre password : </label>
-                <input id="text-password" type="password" class="form-control" name="newpassword">
+                <label for="text-confirmpassword">Confirmer votre mot de passe : </label>
+                <input oninput="password_verify(this)" id="text-confirmpassword" type="password" class="form-control" name="confirmpassword">
+                <div class="invalid-feedback">
+                    Les mots de passes sont différents  
+                </div>
               </div>
 			  <div class="form-group">
-                <label for="pdp">Password : </label>
-                <input id="text-password" type="file" class="form-control" name="fileToUpload">
+                <label for="pdp">Avatar : </label>
+                <input id="file-upload" type="file" class="form-control" name="fileToUpload">
+                <small id="Avatarhelp" class="form-text text-muted">fichier .png .jpg .gif de taille 250x250</small>
               </div>
               <div class="form-group m-0">
-                <button name="action" value="Signin" type="submit" class="btn btn-primary btn-block">Créer mon compte</button>
+                <button id="signupButton" name="action" value="Signup" type="submit" class="btn-primary btn-block">Créer mon compte</button>
               </div>
             </form>
           </div>
