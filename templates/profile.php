50d7978bf67582a80fd3eb0c46891b16b2a4d7f7
LukasG62 <lukas.grando.62@gmail.com>
2022-06-02 14:03:19 +0200

    correction profile.php

--- templates/profile.php
+++ templates/profile.php
@@ -10,6 +10,7 @@ $urlBase = dirname($_SERVER["PHP_SELF"]) . "/index.php";
 if(!($idUser = valider("id"))) rediriger($url,["view"=>"accueil"]);
 if(!$pseudo = getUserPseudo($idUser)) rediriger($url,["view"=>"accueil"]);
 
+$connected = isUserConnected($idUser);
 $rank = getGradeLabel($idUser);
 $bio = bbcodeparser(htmlspecialchars(getUserBio($idUser)));
 $imgPath = $uploadInfo["USERSPATH"];
@@ -25,22 +26,22 @@ $collectionBySeriesFav = groupby($collectionRawFav, "mid");
 
 <div class="container mv-pagebase">
 	<div class="row">
-	<div class="col-0">
-		<img src="<?=$imgPath?>" alt="User avatar" class="photoProfil">
-	</div>
-	<div class="col">
-		<h3><?php echo($pseudo);?></h3>
-		<p>Rank : <?php echo($rank);?> </p>
-		<div class="bio">
-			<p><?php echo($bio);?></p>
+		<div class="col-0">
+			<img src="<?=$imgPath?>" alt="User avatar" class="photoProfil">
+		</div>
+		<div class="col">
+			<h3><img src="ressources/img/p_con_<?=$connected?>.gif"> <?php echo($pseudo);?></h3>
+			<p>Rank : <?php echo($rank);?> </p>
+			<div class="bio">
+				<p><?php echo($bio);?></p>
+			</div>
 		</div>
 	</div>
-
 	<div>
 		<h4 id="titre-col" class="">Ma collection</h4>
 		<br>
 		<?php if($collectionRaw) echo mkCollection($collectionBySeries, $myprofile)?>
-		<h4 class="col-2">Mes Favoris</h4>
+		<h4>Mes Favoris</h4>
 		<?php if($collectionRawFav) echo mkCollection($collectionBySeriesFav, $myprofile, 1)?>
 
 		<br/>
