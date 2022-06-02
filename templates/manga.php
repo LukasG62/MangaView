fbb1fc92342ff51e50f0fff47b4fbda13e21884f
LukasG62 <lukas.grando.62@gmail.com>
2022-06-02 02:01:15 +0200

    modif de structure html + ajout tags

--- templates/manga.php
+++ templates/manga.php
@@ -5,13 +5,15 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 	die("");
 }
 
+	//TODO REDIRIGER QUAND IDMANGA PAS BON
 	$idManga = valider("id", "GET");
 	$dataSerie = getSerieInfos($idManga);
 	$imgPath = $uploadInfo["SERIESPATH"] . $dataSerie[0]['banner'];
+
+	$listTags = getSerieTags($idManga);
 ?>
 
-<div class="container" >
-	<br/>
+<div class="container mv-pagebase" >
 	<h1 class="text_centre"> <?php echo($dataSerie)[0]['titre'] ?></h1>
 	
 	<div class="row"><img src="<?= $imgPath ?>" alt="banner" class="banner"></div>
@@ -20,10 +22,8 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 		$dataFirstTome = getFirstTomeSerie($idManga);
 		$imgPath = $uploadInfo["VOLUMESPATH"] . $dataFirstTome[0]['cover'];
 	?>
-	<br>
-	<br>
-	<br>
-	<div class="row">
+
+	<div class="row mv-manga-info">
 		<div class="col-0">
 			<img src="<?= $imgPath ?>" alt="First Tome's cover" class="couverturedeu" >
 		</div>
@@ -44,26 +44,52 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 			</div>
 		</div>
 	</div>
-</div>
+	<div id="tags">
+		<div class="card">
+		<div class="card-header" id="TagsHeader">
+			<h5 class="mb-0">
+			<button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseTags" aria-expanded="true" aria-controls="collapseTags">Themes :</button>
+			</h5>
+		</div>
+		<div id="collapseTags" class="collapse show" aria-labelledby="TagsHeader" data-parent="#tags">
+			<div class="card-body">
+			<?php
+			$partionedTagsList = array_chunk($listTags, 10);
+			foreach($partionedTagsList as $dataTagsList) {
+				echo "<div class=\"row\">";
+				foreach($dataTagsList as $dataTags) {
+				echo "<div class=\"col\">";
+				echo "<a href=index.php?view=series&tag[]=$dataTags[tid]><button class=\" rounded btn btn-dark\">$dataTags[label]</button></a>";
+				echo "</div>";
+				}
 
-<div class="container">
-	<?php
-		if(valider("connecte", "SESSION")) {
-			echo '<div class="mv-postcomment">';
-			echo mkForm();
-			echo '<div class="form-group">';
-			echo '<label for="exampleFormControlTextarea1">Poster un commentaire :</label>';
-			echo '<textarea name="comment" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>';
-			echo mkButton("submit","action","writeComment", "Poster", "class=\"btn btn-primary form-control\"");
-			echo mkInput("hidden", "type", "serie");
-			echo mkInput("hidden", "id", $idManga);
-		  	echo '</div>';
-			echo endForm();
-			echo '<div class="mv-postcomment">';
-		}
-	
-	?>
+				echo "</div>";
+			}
+			?>
+			</div>
+		</div>
+		</div>
+	</div>
+	<div class="mv-postcomment-container">
+		<?php
+			if(valider("connecte", "SESSION")) {
+				echo '<div class="mv-postcomment">';
+				echo mkForm();
+				echo '<div class="form-group">';
+				echo '<label for="exampleFormControlTextarea1">Poster un commentaire :</label>';
+				echo '<textarea name="comment" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>';
+				echo mkButton("submit","action","writeComment", "Poster", "class=\"btn btn-primary form-control\"");
+				echo mkInput("hidden", "type", "serie");
+				echo mkInput("hidden", "id", $idManga);
+				echo '</div>';
+				echo endForm();
+				echo '</div>';
+			}
+		
+		?>
+	</div>
 	<h2>Les commentaires :</h2>
+	<div class="mv-comment-container">
 	<?php
 		$dataComment = getComments($idManga, 'serie');
 		foreach($dataComment as $dataOneComment){
@@ -71,7 +97,9 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 			echo($comment);
 		}
 	?>
+	</div>
 </div>
+
 
 
 
